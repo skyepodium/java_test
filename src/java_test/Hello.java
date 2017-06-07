2d5a4b4d40d1598c78e562d5ea2a47a05ebdc1ed
김정윤 <skyepodium@gmail.com>
2017-06-07 23:22:09 +0900

    06.07

--- src/java_test/Hello.java
+++ src/java_test/Hello.java
@@ -3,27 +3,11 @@ import java.util.*;
 
 public class Hello {
 	public static void main (String args[]){
-	      Scanner sc = new Scanner(System.in);
-	      int M, N;
-	      M = sc.nextInt();
-	      N = sc.nextInt();
+		
+		String b = "Hello";
+		System.out.println(b);
+		System.out.printf("%s", b);
+		System.out.printf("%s", b);
 
-	      int c = 0;
-	      int d = 0;
-	      for (int i = 0; i <= N; i++){
-	    	  if( i*i >= M && i*i <= N){
-	    		  if( c == 0){
-	    			  d = i*i;
-	    		  }
-	    	  c = c + i*i; 
-	    	  }
-	      }
-	      if(c != 0){
-	      System.out.println(c);
-	      System.out.println(d);
-	      }else{
-	    	  System.out.println(-1);
-	      }
-	      //System.out.println(M);
 	}
 }
